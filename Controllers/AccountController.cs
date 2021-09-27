a981b27773440e25e2a90339ebd5fecb57a14cda
Yousef500 <Kazuya14725@gmail.com>
2021-09-27 21:26:05 +0300

    End of Section #4

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,15 +13,18 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
+
+        private readonly ITokenService _tokenService;
         // GET
 
-        public AccountController(DataContext context)
+        public AccountController(DataContext context, ITokenService tokenService)
         {
             _context = context;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
@@ -34,23 +38,34 @@ namespace API.Controllers
             _context.Users.Add(user);
 
             await _context.SaveChangesAsync();
-            return user;
+            return new UserDto()
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
-        [HttpPost('login')]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
-          var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
 
-          if (user == null) return Unauthorized("Invalid username or password");
+            if (user == null) return Unauthorized("Invalid username or password");
 
-          using var hmac = new HMACSHA512(user.PasswordSalt);
-          
-          var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            using var hmac = new HMACSHA512(user.PasswordSalt);
 
-          for (int i = 0; i < hash.Length; i++) {
-            if (hash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password")
-          }
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
+            for (int i = 0; i < hash.Length; i++)
+            {
+                if (hash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password");
+            }
+
+            return new UserDto()
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
